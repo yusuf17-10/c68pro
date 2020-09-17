0ee31fce9c17a3b927b02b5f24a04da958df3f2c
yusuf <vadapallimumtaj123@gmail.com>
2020-09-17 19:49:59 +0530

    c68

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import Fb from './screens/Fb';
+import In from './screens/In';
+import { createAppContainer} from 'react-navigation'; 
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component() {
+  render(){
+    return(
+    <AppContainer/>
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+    Fb:{screen:FbScreen},
+    In:{screen:InScreen},
+
+
+
+});
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
