4e83f93613a3a64133d2cf783c9a94a2ffbe90a2
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-09-19 21:41:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 
-export default class App extends React.Component() {
+export default class App extends React.Component {
   render(){
     return(
     <AppContainer/>
