b3bc66e8387954a344bc8412caf747448ce81f96
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-09-20 12:45:06 +0530

    Update Fb.js

--- screens/Fb.js
+++ screens/Fb.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text,View} from 'react-native';
 
-export default class Fb extends React.Component(){
+export default class Fb extends React.Component{
     render(){
         return(
             <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
